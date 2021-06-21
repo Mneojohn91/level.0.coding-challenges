303ea08759e07a9ad42e1fbd442636955d9b2d7c
Mneojohn91 <85239600+Mneojohn91@users.noreply.github.com>
2021-06-21 14:52:00 +0200

    adding files

---
+++ Task0_3.py
@@ -0,0 +1,8 @@
+def hello(name):
+    return 'Hello,' + name
+
+
+name = 'tshepo'
+print(f' {hello(name)}! ' )
+
+hello("tshepo")
---
+++ Task0_7.py
@@ -0,0 +1,14 @@
+def tempature_Fahrenheit():
+    number = 12
+    #converting degrees celsius to degrees fahrenheit
+    cels= (number*(9/5))+32
+    print(cels, "degrees fahrenheit")
+tempature_Fahrenheit()
+
+def tempature_celsius():
+    number = 95
+    #converting degrees fahrenheit to celsius
+    cels= (number - 32)*5/9
+    print(cels, "degrees celsius")
+tempature_celsius()
+
