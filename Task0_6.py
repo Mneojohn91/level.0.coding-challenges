7c01cb0cce9eda38864dbbc269300b69f8d96dc9
Mneojohn91 <85239600+Mneojohn91@users.noreply.github.com>
2021-06-24 22:12:35 +0200

    deletes spaces and comment

--- Task0_6.py
+++ Task0_6.py
@@ -1,23 +1,15 @@
-# code to find the highest number
+
 
 def maximum(*number):
-    # 3 numbers
-
-
-
+    
     # Finding the highest and showing/printing result
     if number[0] > number[1] and number[0] > number[2]:
-        
         print("highest number: ", number[0])
     elif number[1] > number[0] and number[1] > number[2]:
-       
         print("highest number: ", number[1])
     else:
-        
         print("highest number: ", number[2])
 
 
 number =(25, 60,12)
-
-
 maximum(*number)
