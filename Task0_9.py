8d81eb399442e2378dec664b94c1e0c020a1ebd4
Mneojohn91 <85239600+Mneojohn91@users.noreply.github.com>
2021-06-24 15:10:53 +0200

    corrected code

--- Task0_9.py
+++ Task0_9.py
@@ -2,11 +2,12 @@ def find_vowel(word):
     #used to ignore case sensitity
     # using for to compare it with the string hat contains vowels
     word=word.casefold()
+    vowels = 'aeiou'
 
     check = [each for each in word if each in vowels]
 
     print( ','.join(set(check)))
 
-vowels = 'aeiou'
+
 
 find_vowel('recognized')
