4926adc7a3deea36771128a7d4ab800c3191ee80
Mneojohn91 <85239600+Mneojohn91@users.noreply.github.com>
2021-07-14 15:30:00 +0200

    refactored and fixed code

--- Task0_6.py
+++ Task0_6.py
@@ -1,15 +1,5 @@
-
-
-def maximum(*number):
-    
-    # Finding the highest and showing/printing result
-    if number[0] > number[1] and number[0] > number[2]:
-        print("highest number: ", number[0])
-    elif number[1] > number[0] and number[1] > number[2]:
-        print("highest number: ", number[1])
-    else:
-        print("highest number: ", number[2])
-
-
-number =(25, 60,12)
-maximum(*number)
+def highest_number(*number):
+   highest_numbers = (list(number))
+   highest_numbers.sort()
+   print (highest_numbers[-1])
+highest_number(1,3,2,5,13,8,1,)
