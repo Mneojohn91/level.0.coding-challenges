6625f7abdc8fad8bc34428fd5759657837c9243b
Mneojohn91 <85239600+Mneojohn91@users.noreply.github.com>
2021-07-30 15:12:13 +0200

    modified 0_6 and 0_8

--- Task0_8.py
+++ Task0_8.py
@@ -1,7 +1,13 @@
 def convert_to_time(number):
     minutes = number - (60 * (number // 60))
     hours = number // 60
-    if minutes > 1 and hours > 1:
+    if hours == 0 and minutes==0:
+        print(hours, "hours,", minutes, "minutes")
+    if hours == 1 and minutes==0:
+	    print(hours, "hour,", minutes, "minutes")
+    elif hours == 0 and minutes==1:
+        print(hours,"hours", minutes,"minute")
+    elif minutes > 1 and hours > 1:
         print(hours, "hours,", minutes, "minutes")
     elif minutes == 1 and hours > 1:
         print(hours, "hours,", minutes, "minute")
@@ -15,7 +21,7 @@ def convert_to_time(number):
 
 def main():
 
-    convert_to_time(173)
+    convert_to_time(1)
 
 
 if __name__ == "__main__":
