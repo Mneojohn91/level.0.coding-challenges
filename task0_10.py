1b6027845309c543e6fe2928411ea3d5526e08c2
Mneojohn91 <85239600+Mneojohn91@users.noreply.github.com>
2021-08-03 12:17:51 +0200

    reformated

--- task0_10.py
+++ task0_10.py
@@ -1,10 +1,10 @@
 def common_letter(word1, word2):
     word1 = word1.lower()
     word2 = word2.lower()
-    same_letters=[each for each in word1 if each in word2]
-    same_letters=", ".join(set(same_letters))
-    print("Common letters: ",same_letters)
-        
-    
+    same_letters = [each for each in word1 if each in word2]
+    same_letters = ", ".join(set(same_letters))
+    print("Common letters: ", same_letters)
+
+
 if __name__ == "__main__":
     common_letter("EckardRe", "BeerryE")
