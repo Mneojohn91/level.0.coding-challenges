055c1f7bda37d2b59caa272233489b4740015e2b
Mneojohn91 <85239600+Mneojohn91@users.noreply.github.com>
2021-08-03 12:14:30 +0200

    modified code

--- task0_10.py
+++ task0_10.py
@@ -1,13 +1,10 @@
 def common_letter(word1, word2):
-    word1 = word1.casefold()
-    word2 = word2.casefold()
-    common_word = ""
-    for alphabet in word1:
-        if alphabet in word2:
-            common_word += alphabet
-    print("Common letters: ", ", ".join(common_word), end=" ")
-
-
-
+    word1 = word1.lower()
+    word2 = word2.lower()
+    same_letters=[each for each in word1 if each in word2]
+    same_letters=", ".join(set(same_letters))
+    print("Common letters: ",same_letters)
+        
+    
 if __name__ == "__main__":
-    common_letter("Eckard", "Berry")
+    common_letter("EckardRe", "BeerryE")
--- task0_7.py
+++ task0_7.py
@@ -2,6 +2,7 @@ def convert_to_fahrenheit(degree_celsius):
     temperature = (degree_celsius * (9 / 5)) + 32
     return temperature
 
+
 def convert_to_celsius(degree_fahr):
     tempature = (degree_fahr - 32) * 5 / 9
     return tempature
